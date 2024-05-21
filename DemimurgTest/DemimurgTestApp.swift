9374f57be936995d6d06078590c6b0bb826f5c02
txxxr <viktor.shmanin@yandex.ru>
2024-05-21 14:10:22 +0300

    Initial Commit

---
+++ DemimurgTest/DemimurgTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemimurgTestApp.swift
+//  DemimurgTest
+//
+//  Created by Victor Shmanin on 21.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct DemimurgTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
